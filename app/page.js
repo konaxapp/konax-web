c91b8fee09c470ebe1003dd9cc445f09924c3b20
konaxapp <nsamaniego@asesoria-soluciones.com>
2026-06-01 16:30:43 -0500

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: "40px", fontFamily: "Arial" }}>
+      <h1>KONAX</h1>
+      <p>Plataforma SaaS de gestión comercial, crédito y cobranza.</p>
+    </main>
+  );
+}
