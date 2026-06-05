40d52c6d786ccd48c307b372a3c58a682b4f1429
konaxapp <nsamaniego@asesoria-soluciones.com>
2026-06-05 15:59:25 -0500

    test

--- app/test/page.js
+++ app/test/page.js
@@ -1,1 +1,35 @@
+"use client";
 
+import { useEffect, useState } from "react";
+import { supabase } from "../../lib/supabase";
+
+export default function Test() {
+  const [clientes, setClientes] = useState([]);
+
+  useEffect(() => {
+    cargarClientes();
+  }, []);
+
+  async function cargarClientes() {
+    const { data, error } = await supabase
+      .from("clientes")
+      .select("*");
+
+    if (error) {
+      console.log(error);
+      return;
+    }
+
+    setClientes(data);
+  }
+
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Clientes Supabase</h1>
+
+      <pre>
+        {JSON.stringify(clientes, null, 2)}
+      </pre>
+    </div>
+  );
+}
